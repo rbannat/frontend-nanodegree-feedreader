b6b5dbbf88c43a18af446353686a452692b1e1ab
Rene Bannat <r.bannat@gmail.com>
2016-04-04 21:32:03 +0200

    add feed content tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,7 +43,7 @@ $(function () {
 
 
     describe('The menu', function () {
-        
+
         it('is hidden by default', function () {
             var $body = $('body');
 
@@ -53,7 +53,7 @@ $(function () {
 
         it('changes visibility on menu icon click', function () {
             var $body = $('body'),
-            menuIcon = $('.menu-icon-link');
+                menuIcon = $('.menu-icon-link');
 
             menuIcon.trigger('click');
             expect($body.hasClass('menu-hidden')).toBe(false);
@@ -64,19 +64,36 @@ $(function () {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                done();
+            });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+        });
 
-     /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+        it('are available', function (done) {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+            done();
+        });
+    });
+
+    describe('New Feed Selection', function () {
+
+        var oldContent;
+
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                oldContent = $('.feed').html();
+                loadFeed(1, done);
+            });
+        });
+
+        it('has changed content', function (done) {
+            expect($('.feed').html()).not.toBe(oldContent);
+            done();
+        });
+    });
+
 }());
